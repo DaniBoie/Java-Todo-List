1ad5457aa541e8bcae9ff8b7170a9087dbcb283f
Daniel Ayala <dannyboy3385@gmail.com>
2022-11-25 07:48:41 -0800

    First JFrame !

--- ToDos.java
+++ ToDos.java
@@ -1,8 +1,15 @@
+import javax.swing.JFrame;
+
 class ToDos {
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
-  }
 
+    JFrame frame = new JFrame();
+    frame.setSize(300,400);
+    frame.setTitle("An empty Frame");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+  }
 }
 
